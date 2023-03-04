9b54acbcd2a3d1bd80b442a867fc36d271d4cac8
Alexsandro souza <alexsandrosouzasilva68@gmail.com>
2023-03-04 19:55:01 -0300

    Ajustando estrutura de pastas

--- js/script.js
+++ js/script.js
@@ -2,3 +2,18 @@ const dias = document.getElementById('dias')
 const horas = document.getElementById('horas')
 const minutos = document.getElementById('minutos')
 const segundos = document.getElementById('segundos')
+
+const relogio = setInterval(function time(){
+    let hoje = new Date();
+    let d = hoje.getDay();
+    let h = hoje.getHours();
+    let m = hoje.getMinutes();
+    let s = hoje.getSeconds();
+
+    dias.textContent = d;
+    horas.textContent = h;
+    minutos.textContent = m;
+    segundos.textContent = s;
+
+})
+
