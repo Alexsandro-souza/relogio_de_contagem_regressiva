5cd9fecdb279f37f03658bd4b9fc18971fd4b732
Alexsandro souza <alexsandrosouzasilva68@gmail.com>
2023-03-05 23:30:23 -0300

    Finalizando logica e css

--- Readme/readme.md
+++ Readme/readme.md
@@ -4,10 +4,8 @@
 <p>Mostrar Dias, Horas, Minutos e Segundo</p> 
 
 ## Definir estilo do relogio
-<p> </p>
-
-![pulso-de-disparo-da-contagem-regressiva-e-interface-utilizador-r-143081337](https://user-images.githubusercontent.com/115429488/222620923-06fdcdc8-a9f5-4c26-acbc-d1228f826048.png)
 
 ## Definir interacao com o usuario
 
    <p>Usuario devera inputar quanto tempo deseja regredir</p>
+   
--- js/script.js
+++ js/script.js
@@ -2,31 +2,43 @@ const clickStart = document.getElementById('btn-start');  //Caputei o botão no 
 const clickPause = document.getElementById('btn-pause');  //Caputei o botão no html
 const clickReset = document.getElementById('btn-reset');  //Caputei o botão no html
 
+
+const horas = document.getElementById('hour');
+const minutos = document.getElementById('minute');
+const segundos = document.getElementById('second');
+
+
+
 // Criei uma função após um evento "Click no botão start"
 clickStart.addEventListener('click', () => {
-  const horas = document.getElementById('hour');
-  const minutos = document.getElementById('minute');
-  const segundos = document.getElementById('second');
-
-  let duracao = (parseInt(horas.value) * 60 * 60) + (parseInt(minutos.value) * 60) + parseInt(segundos.value);
+   let duracao = (parseInt(horas.value) * 60 * 60) + (parseInt(minutos.value) * 60) + parseInt(segundos.value);
 
   visualizarTemporizador = document.getElementById('timer');
   
   timer1(duracao, visualizarTemporizador);
 })
 
-clickPause.addEventListener('click', () => {
-  clearInterval(interval)
-})
+// - Não conseguir fazer o botão de pause funcionar
+/*Cclick no botão pause
+  clickPause.addEventListener('click', () => {
+  (interval)
+})*/
 
-//clickReset.addEventListener('click', () => {
-  //clearInterval(interval);
-  //visualizarTemporizador = "00:00:00"
-//})
+// - Não conseguir fazer o botão de reset funcionar
+/* Cclick no botão reset
+clickReset.addEventListener('click', () => {
+  horas= "00"
+  minutos = "00"
+  segundos = "00"
+  stop(interval)
+  visualizarTemporizador = "00:00:00"
+})*/
+
+
 
 
 const timer1 = (duracao, visualizarTemporizador) => {
-  let tempo = duracao;
+  let tempo = duracao; // Quantidade de seg calculada na função clicStart
   let horas, minutos, segundos;
 
   let interval = setInterval(() => {
@@ -34,19 +46,25 @@ const timer1 = (duracao, visualizarTemporizador) => {
     minutos = Math.floor(tempo / 60 - (horas * 60));
     segundos = Math.floor(tempo % 60);
 
-    console.log(timer1);
-
     horas1 = horas < 10 ? '0' + horas : horas;
     minutos1 = minutos < 10 ? '0' + minutos : minutos;
     segundos1 = segundos < 10 ? '0' + segundos : segundos;
 
+    
     visualizarTemporizador.innerHTML = horas1 + ":" + minutos1 + ":" +segundos1;
     tempo -= 1;
+    console.log(tempo)
 
-    if(timer < 0){
-      visualizarTemporizador.display.innerHTML = 'ACABOU!!!';
+    if(tempo < 1){
       clearInterval(interval);
+      visualizarTemporizador.innerHTML = '00:00:00';
+          
     }
+
+    
+   
   }, 1000);
+
+  
 }
     
